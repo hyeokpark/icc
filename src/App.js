49e8cb128b6c072429ec7d251606e407930a5745
hyeokpark <hyeokpark92@gmail.com>
2021-04-03 18:46:13 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import logo from './images/logo.png';
 import './App.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div style={{ height: '100%' }} className="content">
+        <div style={{ width: '100%', marginTop: '0px' }}>
+          <div style={{ isplay: 'flex', flexDirection: 'column' }}>
+          <div class="intro__SubTitle-sc-10r6k3u-1 jzEppV">테스트</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
