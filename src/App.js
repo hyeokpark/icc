ab1ef286a3ead62dec9966a61dcac80b67ff6baa
hyeokpark <hyeokpark92@gmail.com>
2021-04-03 19:21:54 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <div style={{ height: '100%' }} className="content">
         <div style={{ width: '100%', marginTop: '0px' }}>
           <div style={{ isplay: 'flex', flexDirection: 'column' }}>
-          <div class="jzEppV"><label style={{color:"#3c2212"}}>크리스천</label> <label style={{color:"#8e4d2d"}}>연애</label> <label style={{color:"#3c2212"}}>준비학교 1기</label></div>
+          <div class="jzEppV"><label style={{color:"#3c2212"}}>크리스천</label> <label style={{color:"#8e4d2d"}}>연애</label><br /> <label style={{color:"#3c2212"}}>준비학교 1기</label></div>
           {/* <img src={logo}/> */}
           </div>
         </div>
