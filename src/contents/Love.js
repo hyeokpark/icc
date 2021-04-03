ef95357ec5f3d94235a22448653f02cd9e37666c
hyeokpark <hyeokpark92@gmail.com>
2021-04-04 02:36:42 +0900

    commit

--- src/contents/Love.js
+++ src/contents/Love.js
@@ -1,8 +1,7 @@
 import '../App.css';
 import { useCallback, useEffect, useMemo, useState } from 'react';
 import LoveConfig from '../config/LoveConfig';
-
-
+import LinearProgress from '@material-ui/core/LinearProgress';
 
 function Love(props) {
 
@@ -21,7 +20,7 @@ function Love(props) {
     }, [loveCnt]);
 
     const handleClick = useCallback(() => {
-        if (loveCnt < lCfg.length) {
+        if (loveCnt < lCfg.length - 1) {
             setLoveCnt(loveCnt + 1);
         }
     }, [loveCnt]);
@@ -29,15 +28,16 @@ function Love(props) {
     return (
         <>
             <div style={{ marginBottom: '80px', width: '100%' }}>
-                <button onClick={handleClick} value={q1.type} className="common__DefaultButton-tzij20-2 cdNgSy btn-on-click">
+                <button onClick={handleClick} value={q1.type} className="cdNgSy">
                     <div className="card__ButtonIndex-sc-1uagafc-4 bqQQkU">A</div>
                     <div className="card__ButtonText-sc-1uagafc-5 ltnna">{q1.text}</div>
                 </button>
-                <button onClick={handleClick} value={q2.type} className="common__DefaultButton-tzij20-2 cdNgSy btn-on-click">
+                <button onClick={handleClick} value={q2.type} className="cdNgSy">
                     <div className="card__ButtonIndex-sc-1uagafc-4 bqQQkU">B</div>
                     <div className="card__ButtonText-sc-1uagafc-5 ltnna">{q2.text}</div>
                 </button>
             </div>
+            <LinearProgress color="secondary"  variant="determinate" value={loveCnt / (lCfg.length - 1) * 100} />
 
             <div className="jzEppV" style={{ fontSize: '14px', color: '#a2a2a2' }}>Born to be Loved</div>
         </>
