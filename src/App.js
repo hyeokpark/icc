9a6b8f19c747c416e4803504443d9f2d7312266a
hyeokpark <hyeokpark92@gmail.com>
2021-04-04 00:39:10 +0900

    s

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ import './App.css';
 function App() {
   return (
     <div className="main">
-      <div style={{ height: '100%' }} className="content">
+      <div style={{ height: '100%', marginTop: '50px' }} className="content">
         <div style={{ width: '100%', marginTop: '0px' }}>
           <div style={{ isplay: 'flex', flexDirection: 'column' }}>
-          
-          <div class="jzEppV"><label style={{color:"#3c2212"}}>크리스천</label> <br /> <label style={{color:"#8e4d2d"}}>연애</label><label style={{color:"#3c2212"}}>준비학교</label></div>
-           <button class="ianmHN" style={{backgroundColor:"#f399a5"}}>5가지 사랑의 언어 테스트</button>
-           <button class="ianmHN" style={{backgroundColor:"#7abdc4"}}>5가지 사과의 언어 테스트</button>
+
+            <div class="jzEppV"><label style={{ color: "#3c2212" }}>크리스천</label> <br /> <label style={{ color: "#8e4d2d" }}>연애</label><label style={{ color: "#3c2212" }}>준비학교</label></div>
+            <button class="ianmHN" style={{ backgroundColor: "#f399a5" }}>5가지 사랑의 언어 테스트</button>
+            <button class="ianmHN" style={{ backgroundColor: "#7abdc4" }}>5가지 사과의 언어 테스트</button>
           </div>
         </div>
       </div>
