b023f8137ffd370a1d0143e0019a96c8c174e6e4
hyeokpark <hyeokpark92@gmail.com>
2021-04-04 00:06:18 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './images/logo.jpeg';
+import bornTo from './images/1.png';
 import './App.css';
 
 
@@ -9,8 +10,9 @@ function App() {
       <div style={{ height: '100%' }} className="content">
         <div style={{ width: '100%', marginTop: '0px' }}>
           <div style={{ isplay: 'flex', flexDirection: 'column' }}>
-          <div class="jzEppV"><label style={{color:"#3c2212"}}>크리스천</label> <label style={{color:"#8e4d2d"}}>연애</label><br /> <label style={{color:"#3c2212"}}>준비학교 1기</label></div>
-          {/* <img src={logo}/> */}
+          <img src={bornTo}/> 
+          <div class="jzEppV"><label style={{color:"#3c2212"}}>크리스천</label> <br /> <label style={{color:"#8e4d2d"}}>연애 </label><label style={{color:"#3c2212"}}>준비학교</label></div>
+           
           </div>
         </div>
       </div>
