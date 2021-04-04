c2df893842902a9ff22fae0f7cdc1eff9420f498
hyeokpark <hyeokpark92@gmail.com>
2021-04-04 19:04:30 +0900

    commit

--- src/contents/LoveResult.js
+++ src/contents/LoveResult.js
@@ -2,7 +2,7 @@ import '../App.css';
 import { useCallback, useEffect, useMemo, useState } from 'react';
 import LoveResultConfig from '../config/LoveResultConfig';
 
-import { VictoryAxis, VictoryBar, VictoryChart, VictoryLabel, VictoryPolarAxis } from "victory";
+import { VictoryAxis, VictoryBar, VictoryChart } from "victory";
 
 function LoveResult(props) {
     const loveResultConfig = useMemo(() => {
@@ -25,13 +25,21 @@ function LoveResult(props) {
                 max = props.result[key];
                 result = key;
             }
-
-            t.push({ title: loveResultConfig[key].title.split('(')[0], cnt: props.result[key] });
         }
 
         setResultTitle(loveResultConfig[result].title);
 
         setResultContents(loveResultConfig[result].contents);
+
+
+        for (let key in loveResultConfig) {
+            let value = 0;
+            if (props.result[key]) {
+                value = props.result[key];
+            }
+
+            t.push({ title: loveResultConfig[key].title.split('(')[0], cnt: value });
+        }
 
         setTotalArr(t);
     }, []);
@@ -41,8 +49,11 @@ function LoveResult(props) {
             <div className="jzEppV" style={{ fontSize: '20px', marginBottom: '10px' }}>
                 당신의 사랑의 언어는?
             </div>
-            <div className="jzEppV" style={{ fontSize: '24px', marginBottom: '10px', padding: '10px 10px', color: '#f399a5', fontWeight: 'bold' }}>
-                {resultTitle}
+            <div className="jzEppV" style={{ fontSize: '24px', marginBottom: '10px', padding: '0px 10px', color: '#f399a5', fontWeight: 'bold', marginBottom: '0px' }}>
+                {resultTitle.split('(')[0]}
+            </div>
+            <div className="jzEppV" style={{ fontSize: '15px', marginBottom: '10px', padding: '0px 10px', color: '#f399a5', fontWeight: 'bold' }}>
+                {'(' + resultTitle.split('(')[1]}
             </div>
             <div className="jzEppV" style={{ fontSize: '15px', textAlign: 'left', padding: '10px 10px', marginBottom: '10px' }}>
                 {resultContents}
@@ -52,8 +63,13 @@ function LoveResult(props) {
             <VictoryChart domainPadding={20} animate={{
                 duration: 500
             }}>
-                <VictoryAxis />
-                <VictoryBar labels={({ datum }) => datum.cnt} data={totalArr} x="title" y="cnt" />
+                <VictoryAxis style={{
+                    axis: { stroke: "#595959" },
+                    tickLabels: { fill: "#595959" }
+                }} />
+                <VictoryBar labels={({ datum }) => datum.cnt} data={totalArr} x="title" y="cnt" style={{
+                    data: { fill: "#595959" }, labels: { fill: "#595959" }
+                }} />
             </VictoryChart>
         </>
     );
